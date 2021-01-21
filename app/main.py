c43232f31072500f4bdb09d46bd63c34fcf74d0a
Julio <jn5.guape@gmail.com>
2021-01-20 22:29:56 -0300

    importando modelagem no main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI, Depends
 from app.config import get_settings, Settings
 
+from tortoise.contrib.fastapi import register_tortoise
+
 app = FastAPI()
+
+register_tortoise(
+    app,
+    db_url=os.environ.get("DATABASE_URL"),
+    modules={"models": ["app.models.tortoise"]},
+    generate_schemas=True,
+    add_exception_handlers=True,
+)
 
 
 @app.get("/ping")
