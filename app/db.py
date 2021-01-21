aad0a4004631801c42f6d04c5b03bd8e550e8220
Julio <jn5.guape@gmail.com>
2021-01-20 23:28:43 -0300

    função para criação de db e manipuladores de evento, ao iniciar a aplicação e ao finalziar

---
+++ app/db.py
@@ -0,0 +1,15 @@
+import os
+
+from fastapi import FastAPI
+from tortoise import Tortoise
+from tortoise.contrib.fastapi import register_tortoise
+
+
+def init_db(app: FastAPI) -> None:
+    register_tortoise(
+        app,
+        db_url=os.environ.get("DATABASE_URL"),
+        modules={"models": ["app.models.tortoise"]},
+        generate_schemas=False,
+        add_exception_handlers=True,
+    )
--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 import os
+import logging
 
-from fastapi import FastAPI, Depends
-from app.config import get_settings, Settings
-
+from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
 
 from app.api import ping
+from app.db import init_db
 
+log = logging.getLogger("uvicorn")
 
 def create_application() -> FastAPI:
     application = FastAPI()
@@ -25,3 +26,13 @@ def create_application() -> FastAPI:
 
 
 app = create_application()
+
+@app.on_event("startup")
+async def startup_event():
+    log.info("Starting up...")
+    init_db(app)
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    log.info("Shutting down...")
