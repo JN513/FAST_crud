f392437aece98c28ee839d3f6d5e9e21796310d9
Julio <jn5.guape@gmail.com>
2021-01-20 21:35:08 -0300

    black .

--- app/main.py
+++ app/main.py
@@ -9,5 +9,5 @@ def pong(settings: Settings = Depends(get_settings)):
     return {
         "ping": "pong!",
         "environment": settings.environment,
-        "testing": settings.testing
+        "testing": settings.testing,
     }
