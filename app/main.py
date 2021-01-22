e4ad6be0850b19f63e5ef71f1dcb4548edbedc61
Julio <jn5.guape@gmail.com>
2021-01-22 10:04:13 -0300

    arruamndo main

--- app/main.py
+++ app/main.py
@@ -4,28 +4,26 @@ import logging
 from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
 
-from app.api import ping
+from app.api import ping, summaries
 from app.db import init_db
 
+
 log = logging.getLogger("uvicorn")
+
 
 def create_application() -> FastAPI:
     application = FastAPI()
 
-    register_tortoise(
-        application,
-        db_url=os.environ.get("DATABASE_URL"),
-        modules={"models": ["app.models.tortoise"]},
-        generate_schemas=True,
-        add_exception_handlers=True,
-    )
-
     application.include_router(ping.router)
+    application.include_router(
+        summaries.router, prefix="/summaries", tags=["summaries"]
+    )
 
     return application
 
 
 app = create_application()
+
 
 @app.on_event("startup")
 async def startup_event():
